742baa5d9050d83fb48f1b113cf0cd14ab83b502
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-07-16 13:36:15 -0500

    add timestamps to User model

--- models/user.js
+++ models/user.js
@@ -4,33 +4,26 @@ const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-	fullname: { type: String, default: '' },
-	username: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	password: {
-		type: String,
-		required: true
-	}
-});
+  fullname: { type: String, default: '' },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+}, { timestamps: true });
 
 userSchema.set('toObject', {
-	transform: function(doc, ret) {
-		ret.id = ret._id;
-		delete ret._id;
-		delete ret.__v;
-		delete ret.password;
-	}
+  transform: function (doc, ret) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+    delete ret.password;
+  }
 });
 
-userSchema.methods.validatePassword = function(password) {
-	return bcrypt.compare(password, this.password);
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, this.password);
 };
 
-userSchema.statics.hashPassword = function(password) {
-	return bcrypt.hash(password, 10);
+userSchema.statics.hashPassword = function (password) {
+  return bcrypt.hash(password, 10);
 };
 
 module.exports = mongoose.model('User', userSchema);
