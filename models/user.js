1f18a9c688646939e92dc8cffbb25b658cee4de8
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-07-18 23:44:05 -0500

    Update user model: firstname, lastname instead of fullname and friends with mongoose-friends package

--- models/user.js
+++ models/user.js
@@ -2,28 +2,32 @@
 
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
+const friends = require('mongoose-friends');
 
 const userSchema = new mongoose.Schema({
-	fullname: { type: String, default: '' },
-	username: { type: String, required: true, unique: true },
-	password: { type: String, required: true }
+  firstname: { type: String, default: '' },
+  lastname: { type: String, default: '' },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 }, { timestamps: true });
 
+userSchema.plugin(friends());
+
 userSchema.set('toObject', {
-	transform: function(doc, ret) {
-		ret.id = ret._id;
-		delete ret._id;
-		delete ret.__v;
-		delete ret.password;
-	}
+  transform: function (doc, ret) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+    delete ret.password;
+  }
 });
 
-userSchema.methods.validatePassword = function(password) {
-	return bcrypt.compare(password, this.password);
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, this.password);
 };
 
-userSchema.statics.hashPassword = function(password) {
-	return bcrypt.hash(password, 10);
+userSchema.statics.hashPassword = function (password) {
+  return bcrypt.hash(password, 10);
 };
 
 module.exports = mongoose.model('User', userSchema);
