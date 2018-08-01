475e431d3492fd61862644d9c49f50132e55ce39
Hiram Cruz <hiram0101@gmail.com>
2018-08-01 11:50:01 -0400

    added senderUsername and receiverUsername to request objects response

--- routes/challenges.js
+++ routes/challenges.js
@@ -3,10 +3,13 @@ const router = require('express').Router()
 const passport = require('passport')
 
 // PROTECTION FOR THE FOLLOWING ENDPOINTS
-router.use('/', passport.authenticate('jwt', {
-  session: false,
-  failWithError: true
-}))
+router.use(
+  '/',
+  passport.authenticate('jwt', {
+    session: false,
+    failWithError: true,
+  }),
+)
 
 // Get all challenges for user
 // Has option status query for getting by status
@@ -16,11 +19,85 @@ router.get('/', (req, res, next) => {
 
   if (status) {
     User.getOfStatus(userId, status.toLowerCase())
-      .then(results => res.json(results))
+      .then(results => {
+        results = results.map(async val => {
+          const newVal = { ...val.toObject() }
+          let user
+          if (val.sender != userId + '') {
+            return User.findById(val.sender)
+              .then(doc => {
+                user = doc
+                return Promise.resolve()
+              })
+              .then(() => {
+                newVal.senderUsername = user.username
+                return Promise.resolve()
+              })
+              .then(() => {
+                newVal.receiverUsername = req.user.username
+                return Promise.resolve()
+              })
+              .then(() => Promise.resolve(newVal))
+          } else {
+            return User.findById(val.receiver)
+              .then(doc => {
+                user = doc
+                return Promise.resolve()
+              })
+              .then(() => {
+                newVal.senderUsername = req.user.username
+                return Promise.resolve()
+              })
+              .then(() => {
+                newVal.receiverUsername = user.username
+                return Promise.resolve()
+              })
+              .then(() => Promise.resolve(newVal))
+          }
+        })
+        Promise.all(results).then(stuff => res.json(stuff))
+      })
       .catch(next)
   } else {
     User.getRequests(userId)
-      .then(results => res.json(results))
+      .then(results => {
+        results = results.map(async val => {
+          const newVal = { ...val.toObject() }
+          let user
+          if (val.sender != userId + '') {
+            return User.findById(val.sender)
+              .then(doc => {
+                user = doc
+                return Promise.resolve()
+              })
+              .then(() => {
+                newVal.senderUsername = user.username
+                return Promise.resolve()
+              })
+              .then(() => {
+                newVal.receiverUsername = req.user.username
+                return Promise.resolve()
+              })
+              .then(() => Promise.resolve(newVal))
+          } else {
+            return User.findById(val.receiver)
+              .then(doc => {
+                user = doc
+                return Promise.resolve()
+              })
+              .then(() => {
+                newVal.senderUsername = req.user.username
+                return Promise.resolve()
+              })
+              .then(() => {
+                newVal.receiverUsername = user.username
+                return Promise.resolve()
+              })
+              .then(() => Promise.resolve(newVal))
+          }
+        })
+        Promise.all(results).then(stuff => res.json(stuff))
+      })
       .catch(next)
   }
 })
